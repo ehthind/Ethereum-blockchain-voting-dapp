c26f433e74c5a163111eecfaf05e9b450aefdcab
Amrit <ehthind@gmail.com>
2018-01-21 19:17:32 -0500

    gas amount change, from address added to truffle.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,5 @@ var Voting = artifacts.require("./Voting.sol");
 
 // Deploys contract given, contract name, constructor arguments, and gas
 module.exports = (deployer) => {
-  deployer.deploy(Voting, ['Rama', 'Nick', 'Jose'], {gas: 6700000});
+  deployer.deploy(Voting, ['Rama', 'Nick', 'Jose'], {gas: 470000});
 };
--- truffle.js
+++ truffle.js
@@ -7,7 +7,8 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*',
-      gas: 470000
+      gas: 470000,
+      from: '0xa9ba62da0310ae1e0c5c22ad3f3743dc87b2642e'
     }
   }
 }
